2c1817d04ba3a4d91a2b51e3735ece7201a686ed
Andrew Dollard <andrewdollard@gmail.com>
2017-04-22 22:18:25 -0700

    also push build log to s3

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,5 @@ chown -R builder .
 su -l builder
 time ./build $arch
 
-aws s3 cp --recursive $FIRMWARE_DIR/  s3://amd-sudowrt-builds/
+aws s3 cp $FIRMWARE_DIR/built_firmware/builder.$arch/build.log  s3://amd-sudowrt-builds
+aws s3 cp --recursive $FIRMWARE_DIR  s3://amd-sudowrt-builds
