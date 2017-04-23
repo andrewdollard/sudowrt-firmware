c2336bf17e7584f706e80306f55fbc80d2d65e36
Andrew Dollard <andrewdollard@gmail.com>
2017-04-22 22:47:52 -0700

    trying to use different folders

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,5 @@ then
 fi
 time ./build $arch
 
-aws s3 cp $FIRMWARE_DIR/built_firmware/builder.$arch/build.log  s3://amd-sudowrt-builds
-aws s3 cp --recursive $FIRMWARE_DIR  s3://amd-sudowrt-builds
+aws s3 cp $FIRMWARE_DIR/built_firmware/builder.$arch/build.log  s3://amd-sudowrt-builds/$CI_PLATFORM
+aws s3 cp --recursive $FIRMWARE_DIR  s3://amd-sudowrt-builds/$CI_PLATFORM
