c7ccaae19063cf8251e9bc40ebb41da0ad6e8f5c
Andrew Dollard <andrewdollard@gmail.com>
2017-04-22 19:43:47 -0700

    more codehsip

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-arch=$1
-if [ -z "$arch" ]
-then
-  arch="ar71xx"
-fi
+# arch=$1
+# if [ -z "$arch" ]
+# then
+#   arch="ar71xx"
+# fi
 # chown -R builder .
 # su -l builder
 # time ./build $arch
 
-echo ls ./built_firmware/builder.$arch/
-echo $(ls ./built_firmware/builder.$arch/)
+# echo ls ./built_firmware/builder.$arch/
+# echo $(ls ./built_firmware/builder.$arch/)
+echo $AWS_DEFAULT_REGION
+echo $AWS_ACCESS_KEY_ID
 aws s3 cp $FIRMWARE_DIR/README.md s3://amd-sudowrt-builds/
 # cp -r $FIRMWARE_DIR/built_firmware/builder.$arch/build.log /firmware_images
 # cp -r $FIRMWARE_DIR/built_firmware/builder.$arch/bin /firmware_images
