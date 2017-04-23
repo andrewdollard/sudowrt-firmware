09e29d233420b4f6cbf0d924cd7390a25d82c27e
Andrew Dollard <andrewdollard@gmail.com>
2017-04-22 22:35:53 -0700

    removing builder user crap

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,6 @@ if [ -z "$arch" ]
 then
   arch="ar71xx"
 fi
-chown -R builder .
-su -l builder
 time ./build $arch
 
 aws s3 cp $FIRMWARE_DIR/built_firmware/builder.$arch/build.log  s3://amd-sudowrt-builds
